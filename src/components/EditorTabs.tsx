0857ffff5eaeb30645a47b3bf9dabdbe2e731066
DriveSoft <support@drive-software.com>
2023-02-06 14:59:12 +0200

    Add draggable tabs

--- src/components/EditorTabs.tsx
+++ src/components/EditorTabs.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useRef } from "react";
 import { files } from "../types";
-import './EditorTabs.css'
+import "./EditorTabs.css";
 
 interface EditorTabsProps {
 	fileList: files;
@@ -10,26 +10,74 @@ interface EditorTabsProps {
 	setTabs: (value: string[]) => void;
 }
 
-function EditorTabs({ fileList, activeFile, setActiveFile, tabs, setTabs }: EditorTabsProps) {
-	
-	const onCloseButton = (e: React.MouseEvent<HTMLElement>, fileToClose: string) => {
-		e.stopPropagation(); 
-		setTabs(tabs.filter(fileForClose => fileForClose !== fileToClose))
+function EditorTabs({
+	fileList,
+	activeFile,
+	setActiveFile,
+	tabs,
+	setTabs,
+}: EditorTabsProps) {
+
+	const dragTab = useRef<number | null>(null);
+	const dragOverTab = useRef<number | null>(null);
+
+	const onCloseButton = (
+		e: React.MouseEvent<HTMLElement>,
+		fileToClose: string
+	) => {
+		e.stopPropagation();
+		setTabs(tabs.filter((fileForClose) => fileForClose !== fileToClose));
 		if (activeFile === fileToClose) setActiveFile(undefined);
+	};
+
+	const onDragStart = (e: React.DragEvent<HTMLDivElement>, position: number) => {
+		dragTab.current = position
 	}
+
+	const onDragEnter = (e: React.DragEvent<HTMLDivElement>, position: number) => {
+		dragOverTab.current = position
+		e.preventDefault()
+	}	
+
+	const drop = (e: React.DragEvent<HTMLDivElement>) => {
+		if (dragTab.current !== null && dragOverTab.current !== null) {
+			const copyListItems = [...tabs];
+			const dragItemContent = copyListItems[dragTab.current];
+			copyListItems.splice(dragTab.current, 1); // delete current pos
+			copyListItems.splice(dragOverTab.current, 0, dragItemContent); // insert at dragOverItem position element dragItemContent 
+			dragTab.current = null;
+			dragOverTab.current = null;
+			setTabs(copyListItems);
+			console.log(copyListItems);
+		}
+	};	
 
 	return (
 		<div className="editorTabs">
-			{
-				tabs.map((fileId: string) => (
-					fileList[fileId] && ( 
-					<div key={fileId} className={activeFile === fileId ? "tab activeTab" : "tab" } onClick={()=>setActiveFile(fileId)} >
-						<i className="tabIcon fa-regular fa-file-lines"></i>
-						<p className="tabFilename">{fileList[fileId]}</p>
-						<p className="tabCloseButton" onClick={ (e) => onCloseButton(e, fileId) }>⨉</p>
-					</div>)								
-				))
-			}
+			{tabs.map(
+				(fileId: string, index: number) =>
+					fileList[fileId] && (
+						<div
+							key={fileId}
+							className={ activeFile === fileId ? "tab activeTab" : "tab" }
+							onClick={() => setActiveFile(fileId)}
+							onDragStart={(e) => onDragStart(e, index)}
+							onDragEnter={(e) => onDragEnter(e, index)}
+							onDragOver={(e) => e.preventDefault()}
+							onDragEnd={drop}
+							draggable
+						>
+							<i className="tabIcon fa-regular fa-file-lines"></i>
+							<p className="tabFilename">{fileList[fileId]}</p>
+							<p
+								className="tabCloseButton"
+								onClick={(e) => onCloseButton(e, fileId)}
+							>
+								⨉
+							</p>
+						</div>
+					)
+			)}
 		</div>
 	);
 }
